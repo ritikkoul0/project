2bf78c3b32931386f4bd0f33f6cc4f81933cd180
ritikkoul0 <61817094+ritikkoul0@users.noreply.github.com>
2021-08-08 15:47:54 +0530

    Add files via upload

---
+++ vtu.js
@@ -0,0 +1,17 @@
+function greet(){
+  confirm("submit");
+}
+
+function random()
+    {
+    var a=document.getElementById("state").value;
+    if(a=="Goa")
+    {
+      var arr[]={'goa','panji','neeru'};
+
+    }
+    for(var i=0;i<arr.length();i++)
+    {
+      document.getElementById("district")=arr[i];
+    }
+    }
